d43e34b31006005f3d7cb261f356169ff7bd9e22
Mitch Hartigan <mitch.hartigan@gmail.com>
2021-08-30 18:18:46 -0400

    deleted cra boilerplate, setup router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <p>Homepage</p>
+        </Route>
+        <Route path="/resources">
+          <p>Resources</p>
+        </Route>
+        <Route path="/initiatives">
+          <p>Initiatives</p>
+        </Route>
+        <Route path="/practice-areas">
+          <p>Practice Areas</p>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
