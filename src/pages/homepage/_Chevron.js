70087e415ccee49db973f7970603d3e53a91f53a
mitch <mitch.hartigan@gmail.com>
2021-09-10 11:28:17 -0700

    fix: chevron sliding in after refresh

--- src/pages/homepage/_Chevron.js
+++ src/pages/homepage/_Chevron.js
@@ -6,7 +6,7 @@ export function Chevron(props) {
   const { firstLoad } = props;
 
   return (
-    <Container firstLoad={firstLoad}>
+    <Container>
       <Link
         to="chevronScrollTarget"
         spy={true}
@@ -16,7 +16,7 @@ export function Chevron(props) {
         smooth={true}
         style={{ margin: "0px", padding: "0px" }}
       >
-        <Image src="chevron.png" alt="Chevron icon." />
+        <Image src="chevron.png" alt="Chevron icon." firstLoad={firstLoad} />
       </Link>
     </Container>
   );
@@ -35,14 +35,8 @@ const fadeIn = keyframes`
 
 const Container = styled.div`
   position: absolute;
-  opacity: ${(props) => (props.firstLoad ? "0" : "1")};
   bottom: 30px;
   z-index: 1;
-  transform: translate(${(props) => (props.firstLoad ? "0, 20px" : "0, 0")});
-  animation: ${(props) => (props.firstLoad ? fadeIn : "")};
-  animation-duration: 1s;
-  animation-fill-mode: forwards;
-  animation-delay: 1700ms;
   cursor: pointer;
 
   &: hover {
@@ -51,4 +45,12 @@ const Container = styled.div`
   transition: transform 100ms linear;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  transform: translate(${(props) => (props.firstLoad ? "0, 20px" : "0, 0")});
+  opacity: ${(props) => (props.firstLoad ? "0" : "1")};
+  animation: ${(props) => (props.firstLoad ? fadeIn : "")};
+
+  animation-duration: 1s;
+  animation-fill-mode: forwards;
+  animation-delay: 1700ms;
+`;
