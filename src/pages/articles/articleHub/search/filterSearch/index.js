6a8d2ac541dc547f5ab725b5593627e151143c68
Mitch Hartigan <mitch.hartigan@gmail.com>
2022-02-24 01:43:02 -0800

    feat: controlled tag input elements

--- src/pages/articles/articleHub/search/filterSearch/index.js
+++ src/pages/articles/articleHub/search/filterSearch/index.js
@@ -1,8 +1,35 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 
+import { tags } from "./tags";
+
 export default function FilterSearch() {
   const [dropdown, toggleDropdown] = useState(false);
+  const [activeTags, setActiveTags] = useState([]);
+
+  function addActiveTag(tag) {
+    const updatedTags = [...activeTags, tag];
+    console.log("addActiveTag, updatedTags:", updatedTags);
+    setActiveTags(updatedTags);
+  }
+
+  function removeActiveTag(tag) {
+    const updatedTags = activeTags;
+    const tagIndex = activeTags.indexOf(tag);
+    updatedTags.splice(tagIndex, 1);
+
+    console.log("removeActiveTag, updatedTags: ", updatedTags);
+
+    setActiveTags(updatedTags);
+  }
+
+  function handleCheckInput(evt) {
+    if (evt.target.checked) {
+      addActiveTag(evt.target.name);
+    } else {
+      removeActiveTag(evt.target.name);
+    }
+  }
 
   if (!dropdown) {
     return (
@@ -10,9 +37,54 @@ export default function FilterSearch() {
     );
   }
   return (
-    <FilterDropdown onClick={() => toggleDropdown(false)}></FilterDropdown>
+    <FilterDropdown>
+      <DropDownHeaderContainer>
+        <Title>Filter by tag</Title>
+        <Chevron onClick={() => toggleDropdown(false)} />
+      </DropDownHeaderContainer>
+
+      <Span />
+      {tags.map((tag) => {
+        return (
+          <div>
+            <input
+              type="checkbox"
+              name={tag}
+              onChange={(evt) => handleCheckInput(evt)}
+              key={tag}
+            />
+            <label htmlFor={tag}>{tag}</label>
+          </div>
+        );
+      })}
+    </FilterDropdown>
   );
 }
+
+const Chevron = styled.div`
+  width: 15px;
+  height: 15px;
+  background-color: lightblue;
+`;
+
+const Span = styled.div`
+  width: 100%;
+  height: 2px;
+  position: static;
+  background-color: ${(props) => props.theme.colors.mainGold};
+`;
+
+const Title = styled.h4`
+  margin-bottom: 0px;
+`;
+
+const DropDownHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+`;
 
 const FilterButton = styled.div`
   display: flex;
@@ -22,7 +94,8 @@ const FilterButton = styled.div`
 
 const FilterDropdown = styled.div`
   display: flex;
+  flex-direction: column;
+  justify-content: center;
   width: 300px;
-  height: 500px;
   background-color: white;
 `;
---
+++ src/pages/articles/articleHub/search/filterSearch/tags.js
@@ -0,0 +1,1 @@
+export const tags = ["HMDA", "Oregon", "Forebearance", "Foreclosure"];
