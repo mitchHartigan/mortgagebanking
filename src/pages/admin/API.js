e79d0a161464a12871bb66789c6d6ecac138cada
Mitchell Hartigan <mitch.hartigan@gmail.com>
2022-04-21 12:06:19 -0700

    deploy: swapped localhost to lambda backend

--- src/pages/admin/API.js
+++ src/pages/admin/API.js
@@ -1,31 +1,39 @@
 export const FETCH_PENDING_ACRONYMS = async () => {
-  const result = await fetch("http://localhost:4000/pendingAcronyms");
+  const result = await fetch(
+    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/pendingAcronyms"
+  );
   const json = await result.json();
 
   if (json.pendingAcronyms) return json.pendingAcronyms;
 };
 
 export const REJECT_PENDING_ACRONYM = async (acronym) => {
-  const result = await fetch("http://localhost:4000/rejectPendingAcronym", {
-    method: "POST",
-    body: JSON.stringify(acronym),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const result = await fetch(
+    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/rejectPendingAcronym",
+    {
+      method: "POST",
+      body: JSON.stringify(acronym),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
 
   const json = await result.json();
   return json.deletionConfirmed;
 };
 
 export const ACCEPT_PENDING_ACRONYM = async (acronym) => {
-  const result = await fetch("http://localhost:4000/acceptPendingAcronym", {
-    method: "POST",
-    body: JSON.stringify(acronym),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
+  const result = await fetch(
+    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/acceptPendingAcronym",
+    {
+      method: "POST",
+      body: JSON.stringify(acronym),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
   const json = await result.json();
   return json.acceptConfirmed;
 };
