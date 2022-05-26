b14d3d49add9187d0db0adaddfbb3caf631ed3b2
Mitchell Hartigan <mitch.hartigan@gmail.com>
2022-05-26 11:53:56 -0700

    refactor: API url

--- src/pages/admin/API.js
+++ src/pages/admin/API.js
@@ -1,39 +1,47 @@
+const URL = "http://localhost:4000";
+// const URL =
+//   "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/pendingAcronyms";
+
 export const FETCH_PENDING_ACRONYMS = async () => {
-  const result = await fetch(
-    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/pendingAcronyms"
-  );
+  const result = await fetch(`${URL}/pendingAcronyms`);
   const json = await result.json();
 
   if (json.pendingAcronyms) return json.pendingAcronyms;
 };
 
+export const UPDATE_PENDING_ACRONYM = async (acronym) => {
+  const result = await fetch(`${URL}/pendingAcronyms`, {
+    method: "POST",
+    body: JSON.stringify(acronym),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const json = await result.json();
+  return json.acceptConfirmed;
+};
+
 export const REJECT_PENDING_ACRONYM = async (acronym) => {
-  const result = await fetch(
-    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/rejectPendingAcronym",
-    {
-      method: "POST",
-      body: JSON.stringify(acronym),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const result = await fetch(`${URL}/rejectPendingAcronym`, {
+    method: "POST",
+    body: JSON.stringify(acronym),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
   const json = await result.json();
   return json.deletionConfirmed;
 };
 
 export const ACCEPT_PENDING_ACRONYM = async (acronym) => {
-  const result = await fetch(
-    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/acceptPendingAcronym",
-    {
-      method: "POST",
-      body: JSON.stringify(acronym),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const result = await fetch(`${URL}/acceptPendingAcronym`, {
+    method: "POST",
+    body: JSON.stringify(acronym),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const json = await result.json();
   return json.acceptConfirmed;
 };
@@ -45,16 +53,13 @@ export const CHECK_TOKEN = async () => {
 
   const payload = { token: token };
 
-  const result = await fetch(
-    "https://md5rhmga23.execute-api.us-west-2.amazonaws.com/production/checkAuthentication",
-    {
-      method: "POST",
-      body: JSON.stringify(payload),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }
-  );
+  const result = await fetch(`${URL}/checkAuthentication`, {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
   const json = await result.json();
   return json.validToken;
 };
