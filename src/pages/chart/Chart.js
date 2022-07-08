0753043134c48f10199d2a9121609dcaf5498e13
Mitch Hartigan <mitch.hartigan@gmail.com>
2022-07-08 15:03:59 -0700

    wip: create 3d array

--- src/pages/chart/Chart.js
+++ src/pages/chart/Chart.js
@@ -10,23 +10,53 @@ export default function Chart(props) {
     let value = 0;
 
     async function findSmallestChildren(objArray) {
-      objArray.forEach((obj) => {
-        const hasChildren = obj.children && obj.children.length > 0;
+      if (objArray && objArray.length > 0) {
+        objArray.forEach((obj) => {
+          const hasChildren = obj.children && obj.children.length > 0;
 
-        if (hasChildren) {
-          findSmallestChildren(obj.children);
-        } else {
-          value = value + 1;
-        }
-      });
+          if (hasChildren) {
+            findSmallestChildren(obj.children);
+          } else {
+            value = value + 1;
+          }
+        });
+      }
     }
 
     await findSmallestChildren(obj.children);
+
+    // minimum row size for an object is one. Objects with no children
+    // (before this check) have a row size of 0.
+    if (value === 0) value = 1;
     return value;
   };
 
+  async function genCoordinateArray(objArray) {
+    const genColumnCoords = async (objArray) => {
+      let columnCoords = [];
+      let cursor = 1;
+
+      objArray.forEach(async (obj, i) => {
+        const objRowSize = await findObjRowSize(obj);
+        console.log("obj", obj);
+        console.log("objRowSize", objRowSize);
+
+        const objCoords = [cursor, cursor + objRowSize];
+        cursor = cursor + objRowSize;
+        console.log(objCoords);
+
+        if (obj.children && obj.children.length > 0) {
+          return genColumnCoords(obj.children);
+        }
+      });
+    };
+
+    return await genColumnCoords(objArray);
+  }
+
   useEffect(async () => {
     console.log(await findObjRowSize(data[0]));
+    console.log(await genCoordinateArray(data));
   });
 
   return <Container>chart.</Container>;
