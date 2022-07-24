9b7268bee321a0e367af1c51c0ed2c1bb7e07515
Mitchell Hartigan <mitch.hartigan@gmail.com>
2022-07-24 15:38:57 -0700

    feat: fixed cell title header

--- src/pages/chart/TitleCell.js
+++ src/pages/chart/TitleCell.js
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 
 export default function Cell(props) {
   const { coords, name, type, body, column, isTitle } = props;
+  const [cellWidth, setCellWidth] = useState();
+  const containerRef = useRef();
+
+  useEffect(() => {
+    setCellWidth(containerRef.current.offsetWidth);
+
+    window.addEventListener("resize", () => {
+      setCellWidth(containerRef.current.offsetWidth);
+    });
+  });
 
   return (
-    <Container coords={coords} isTitle={isTitle} column={column}>
+    <Container
+      ref={containerRef}
+      coords={coords}
+      isTitle={isTitle}
+      column={column}
+    >
+      <FixedCell cellWidth={cellWidth}>
+        <Text>{name || body}</Text>
+      </FixedCell>
       <Text>{name || body}</Text>
     </Container>
   );
 }
 
-const Text = styled.p`
+const FixedCell = styled.div`
   position: fixed;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: ${({ cellWidth }) => `${cellWidth}px`};
+  background-color: lightblue;
+`;
+
+const Text = styled.p`
   font-family: ${({ theme }) => theme.textFont};
   font-size: ${({ theme }) => theme.text.md};
   margin: 0px;
-  margin-bottom: 25px;
 `;
 
 const Container = styled.div`
